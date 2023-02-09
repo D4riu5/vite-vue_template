61c0425feca02e49ccab5b4659aa186ca0c62f3b
D4riu5 <R3iji94@gmail.com>
2023-02-09 06:06:29 +0100

    Version 1.2 - added fontawesome icons

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
-// bootstrap components
+// bootstrap css/icons
 import 'bootstrap/dist/css/bootstrap.min.css'
 // bootstrap components
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// fontawesome icons
+import '@fortawesome/fontawesome-free/css/all.css'
 
 // axios is installed, use import axios from 'axios', in the component when you need to use it
 
